fd65fbad7d855668be54226bf87b54f8f208c398
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-02-09 11:39:20 +0200

    Implementing the protocol for sharing data

--- NotesOriginal/NotesOriginal/Note.swift
+++ NotesOriginal/NotesOriginal/Note.swift
@@ -19,11 +19,6 @@ struct Note {
     // Initializer is a method that prepares an instance of a class for use (setting an initial value for each property and performing any other setup or initialization)
     
     init?(name: String) {
-        
-//        // Initialization should fail if there is no name
-//        if (name.isEmpty) {
-//            return nil
-//        }
 
         // The name must not be empty
         guard !name.isEmpty else {
--- NotesOriginal/NotesOriginal/NotesManager.swift
+++ NotesOriginal/NotesOriginal/NotesManager.swift
@@ -8,6 +8,6 @@
 
 import Foundation
 
-class NotesManager {
+protocol NotesManager {
     
 }
