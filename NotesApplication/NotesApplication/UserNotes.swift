3a401c71eb82d528ec0e6ad781a7d883e264ca30
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-03-19 19:17:43 +0200

    Adding the file that is for the notes

---
+++ NotesApplication/NotesApplication/UserNotes.swift
@@ -0,0 +1,26 @@
+//
+//  UserNotes.swift
+//  NotesApplication
+//
+//  Created by Kalin Karev on 3/19/18.
+//  Copyright © 2018 Kalin Karev. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class UserNotes {
+    
+    // Array for storing the notes (all the user notes are stored here)
+    var notes: [Note] = []
+    
+    func addNote(newNote: Note) -> Note {
+        return newNote
+    }
+    
+    func deleteNote(deletedNote: Note) -> Note {
+        return deletedNote
+    }
+    
+}
