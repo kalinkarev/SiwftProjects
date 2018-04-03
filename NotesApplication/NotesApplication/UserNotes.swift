fbd2380d1f69f29a174068e90a05b25c9e3c72e6
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-04-03 11:08:52 +0300

    Refactoring the code

--- NotesApplication/NotesApplication/UserNotes.swift
+++ NotesApplication/NotesApplication/UserNotes.swift
@@ -12,11 +12,8 @@ import UIKit
 struct UserNotes {
     // Array for storing the notes (all the user notes are stored here)
     var notes: [Note] = []
-    
+
     private var counter: Int = 1
-    private var tmp: Int = 1
-    var saveValue: Int = 0
-    var identifier: Int = 0
     
     mutating func loadData() {
         guard let note1 = Note(id: 0, name: "Go to work") else {
@@ -45,23 +42,6 @@ struct UserNotes {
         var localNewNote = newNote
         localNewNote.id = lastID! + 1
         
-        
-//        if let lastNote = notes.last {
-//            print("The last note is: \(lastNote)")
-//
-//            var lastID = lastNote.id
-//            newNote.id = lastID + 1
-//
-//            func someFunction (parameterName:Int) {
-//                var localParameterName = parameterName
-//                // Now use localParameterName
-//                localParameterName = 2;
-//                var a = localParameterName;
-//            }
-            
-//            newNote.id = lastNote.id + 1
-//        }
-        
         notes += [localNewNote]
         
         print("The new note has name: \(localNewNote.name) and id: \(localNewNote.id)")
@@ -79,14 +59,14 @@ struct UserNotes {
         counter = counter + 1
         return counter
     }
-    
-    mutating func printValues() {
-        print("The value of tmp is: \(tmp)")
-        print("The value of the save value is: \(saveValue)")
-        if (identifier == counter) {
-            identifier += 1
-            print("The value of the identifier of the new note is \(identifier)")
-        }
-        // How to make the identifier to be incremented By One each time the user save a note
-    }
+//
+//    mutating func printValues() {
+//        print("The value of tmp is: \(tmp)")
+//        print("The value of the save value is: \(saveValue)")
+//        if (identifier == counter) {
+//            identifier += 1
+//            print("The value of the identifier of the new note is \(identifier)")
+//        }
+//        // How to make the identifier to be incremented By One each time the user save a note
+//    }
 }
