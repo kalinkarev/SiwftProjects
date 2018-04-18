1d3899e4341d8694efee1b9b00bdb22d2fb68019
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-04-18 14:46:49 +0300

    Making the user to enter text

--- ProjectPassingDataFromOneViewToAnother/ProjectPassingDataFromOneViewToAnother/ViewController.swift
+++ ProjectPassingDataFromOneViewToAnother/ProjectPassingDataFromOneViewToAnother/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var noteNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field`s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        noteNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        noteNameLabel.text = "Default Text"
+    }
+    
 }
 
