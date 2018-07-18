5ae398b542b76b4795fdfa23b20ef43589ba5acb
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-07-18 15:31:36 +0300

    Have implemented the separator of string without using reverse

---
+++ StringSeparator/StringSeparation/StringSeparationIWithoutReverse/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  StringSeparationIWithoutReverse
+//
+//  Created by Kalin Karev on 7/17/18.
+//  Copyright © 2018 Kalin Karev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var numberSeparated: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: Actions
+    @IBAction func formatButon(_ sender: UIButton) {
+        let input = inputTextField.text
+        var result = ""
+        
+        let separatedNumbers = numberSeparated.text
+        var number = Int(separatedNumbers!)
+        
+        if number == nil || number == 0 {
+            number = 5
+        }
+        
+        result = formatString(input: input!, number: number!)
+        
+        outputLabel.text = result
+    }
+    
+    func formatString( input: String, number: Int) -> String {
+        var userInput = input
+//        print("The input is: \(String(describing: userInput))")
+        
+        let countSymbols = userInput.count
+//        print("The number of characters in the string are: \(String(describing: countSymbols))")
+        
+        if (countSymbols < 3 || countSymbols > 25) {
+            print("You should enter number between 3 and 25 digits")
+        }
+        
+        let numberOfSeparators = countSymbols / number
+        
+        var numberOfSymbolsBeforeFirstSeparator = countSymbols % number
+//        print("The number of symbols before the first separator is: \(numberOfSymbolsBeforeFirstSeparator)")
+        
+        for _ in 0..<numberOfSeparators {
+            userInput.insert(" ", at: (userInput.index((userInput.startIndex), offsetBy: numberOfSymbolsBeforeFirstSeparator)))
+            numberOfSymbolsBeforeFirstSeparator += number + 1
+        }
+        
+//        print("The result is: \(String(describing: userInput))")
+        return userInput
+    }
+}
