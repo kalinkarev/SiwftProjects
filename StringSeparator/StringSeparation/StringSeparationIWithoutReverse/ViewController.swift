80ce54cfb12bb4eb6b62fbce2028ebe7641d95db
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-07-19 14:09:43 +0300

    Adding the functionality the numbers to be splitted by string

--- StringSeparator/StringSeparation/StringSeparationIWithoutReverse/ViewController.swift
+++ StringSeparator/StringSeparation/StringSeparationIWithoutReverse/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var numberSeparated: UITextField!
     @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var separatorTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +30,12 @@ class ViewController: UIViewController {
     // MARK: Actions
     @IBAction func formatButon(_ sender: UIButton) {
         let input = inputTextField.text
+        var separator = separatorTextField.text
         var result = ""
+        
+        if (input?.count)! < 3 || (input?.count)! > 25 {
+            print("You should enter a number between 3 and 25 digits.")
+        }
         
         let separatedNumbers = numberSeparated.text
         var number = Int(separatedNumbers!)
@@ -37,8 +43,12 @@ class ViewController: UIViewController {
         if number == nil || number == 0 {
             number = 5
         }
+
+        if (separator?.isEmpty)! {
+            separator = " "
+        }
         
-        result = formatString(input: input!, number: number!, separator: " ")
+        result = formatString(input: input!, number: number!, separator: separator!)
         
         outputLabel.text = result
         
@@ -46,22 +56,21 @@ class ViewController: UIViewController {
     }
 }
     
-func formatString( input: String, number: Int, separator: Character) -> String {
+func formatString( input: String, number: Int, separator: String) -> String {
     var userInput = input
 //    print("The input is: \(userInput)")
     let countSymbols = userInput.count
 //    print("The number of characters in the string are: \(countSymbols)")
-    if (countSymbols < 3 || countSymbols > 25) {
-        print("You should enter number between 3 and 25 digits.")
-    }
     
     let numberOfSeparators = countSymbols / number
     
     var numberOfSymbolsBeforeFirstSeparator = countSymbols % number
-//    print("The number of symbols before the first separator is: \(numberOfSymbolsBeforeFirstSeparator)")
+
+    let separatorLenght = separator.count
+    
     for _ in 0..<numberOfSeparators {
-        userInput.insert(separator, at: (userInput.index((userInput.startIndex), offsetBy: numberOfSymbolsBeforeFirstSeparator)))
-        numberOfSymbolsBeforeFirstSeparator += number + 1
+        userInput.insert(contentsOf: separator, at: (userInput.index(userInput.startIndex, offsetBy: numberOfSymbolsBeforeFirstSeparator)))
+        numberOfSymbolsBeforeFirstSeparator += number + separatorLenght
     }
     
 //    print("The result is: \(userInput)")
