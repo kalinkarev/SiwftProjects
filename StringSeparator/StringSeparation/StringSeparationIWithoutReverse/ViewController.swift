26f3806250df48a6191dd9e5c434ac1e656bb489
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-07-23 11:09:49 +0300

    Making the application more clear(Using extension)

--- StringSeparator/StringSeparation/StringSeparationIWithoutReverse/ViewController.swift
+++ StringSeparator/StringSeparation/StringSeparationIWithoutReverse/ViewController.swift
@@ -28,32 +28,48 @@ class ViewController: UIViewController {
 
     // MARK: Actions
     @IBAction func formatButon(_ sender: UIButton) {
+        
+        
+        
         var input = inputTextField.text
         var separator = separatorTextField.text
         var result = ""
-        
-        if (input?.count)! < 3 || (input?.count)! > 25 {
-            print("You should enter a number between 3 and 25 digits.")
-        }
-        
-        let separatedNumbers = numberSeparated.text
-        var number = Int(separatedNumbers!)
-        
-        if number == nil || number == 0 {
-            number = 5
-        }
 
-        if (separator?.isEmpty)! {
-            separator = " "
-        }
+        let separatedNumbers = numberSeparated.text
+
+        result = (input?.textInput(userInput: input!, separator: separator!, groupNumber: separatedNumbers!))!
         
-        result = (input?.formatString(number: number!, separator: separator!))!
         outputLabel.text = result
         self.view.endEditing(true) // hiding the keyboard when the button is pressed
     }
 }
 
 extension String {
+    
+    func textInput( userInput: String, separator: String, groupNumber: String) -> String {
+        var input = userInput
+        var separator = separator
+        var result = ""
+        
+        if input.count < 3 || input.count > 25 {
+            print("You should enter a number between 3 and 25 digits.")
+        }
+        
+        let separatedNumbers = groupNumber
+        var number = Int(separatedNumbers)
+        
+        if number == nil || number == 0 {
+            number = 5
+        }
+        
+        if separator.isEmpty {
+            separator = " "
+        }
+        
+        result = input.formatString(number: number!, separator: separator)
+        return result
+    }
+    
     mutating func formatString( number: Int, separator: String) -> String {
         let numberOfSeparators = self.count / number
         var numberOfSymbolsBeforeFirstSeparator = self.count % number
