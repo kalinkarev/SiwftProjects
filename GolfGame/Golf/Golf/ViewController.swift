81d3ab516906fcf3865b883093920a90c7e355e8
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-07-30 14:54:11 +0300

    Starting golf application

---
+++ GolfGame/Golf/Golf/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Golf
+//
+//  Created by Kalin Karev on 7/30/18.
+//  Copyright © 2018 Kalin Karev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: Actions
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        print("You have pressed the add button")
+    }
+
+}
+
