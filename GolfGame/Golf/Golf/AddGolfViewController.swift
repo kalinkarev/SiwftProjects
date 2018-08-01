2ea69eee67afdf12b74b5cdc25117b1295a058f3
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-08-01 15:50:22 +0300

    Adding the table view in the add golf screen

--- GolfGame/Golf/Golf/AddGolfViewController.swift
+++ GolfGame/Golf/Golf/AddGolfViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class AddGolfViewController: UIViewController {
+class AddGolfViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var sessionNumber: Int = 0
+    // MARK: Properties
+    @IBOutlet weak var numberHolesTableView: UITableView!
+    
+    var numberHoles: Int = 0
+    
+//    var sessionNumber: Int = 0
+//    var sections: Int = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        showAlert()
+        numberHolesTableView.register(AddGolfTableViewCell.self, forCellReuseIdentifier: "cellAdd")
+        
+        numberHolesTableView.isHidden = true
+        
         showActionSheet()
     }
-
+    
+    func showTableView() {
+        numberHolesTableView.isHidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,47 +46,6 @@ class AddGolfViewController: UIViewController {
     
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
         print("You have pressed the save button")
-        print("The number is: \(sessionNumber)")
-    }
-    
-    func showAlert() {
-        
-        let alert = UIAlertController(title: "Choose how many dupki do you want",
-                                      message: "Submit something",
-                                      preferredStyle: .alert
-        )
-        
-        // Submit button
-        let submitAction = UIAlertAction(title: "Submit", style: .default, handler: {
-            (action) -> Void in
-            // Get 1st TextField`s text
-            let textField = alert.textFields![0]
-            
-            let number = Int(textField.text!)
-            
-            self.sessionNumber = number!
-            
-            self.showTexts()
-        })
-        
-        // Cancel button
-        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: {
-            (action) -> Void in
-        })
-        
-        // Add 1 textField and customize it
-        alert.addTextField { (textField: UITextField) in
-            textField.keyboardAppearance = .dark
-            textField.keyboardType = .default
-            textField.autocorrectionType = .default
-            textField.placeholder = "Type the number of holes you want to have"
-            textField.clearButtonMode = .whileEditing
-        }
-        
-        // Add action buttons and present the Alert
-        alert.addAction(submitAction)
-        alert.addAction(cancel)
-        present(alert, animated: true, completion: nil)
     }
     
     func showActionSheet() {
@@ -82,62 +54,62 @@ class AddGolfViewController: UIViewController {
         let nineButton = UIAlertAction(title: "Nine holes", style: .default, handler: {
             (action) -> Void in
             print("Nine holes")
-            self.sessionNumber = 9
-            self.showTexts()
+            self.numberHoles = 9
+            self.prepopulateTableView()
         })
         let tenButton = UIAlertAction(title: "Ten holes", style: .default, handler: {
             (action) -> Void in
             print("Ten holes")
-            self.sessionNumber = 10
-            self.showTexts()
+            self.numberHoles = 10
+            self.prepopulateTableView()
         })
         let elevenButton = UIAlertAction(title: "Eleven holes", style: .default, handler: {
             (action) -> Void in
             print("Eleven holes")
-            self.sessionNumber = 11
-            self.showTexts()
+            self.numberHoles = 11
+            self.prepopulateTableView()
         })
         let twelveButton = UIAlertAction(title: "Twelve holes", style: .default, handler: {
             (action) -> Void in
             print("Twelve holes")
-            self.sessionNumber = 12
-            self.showTexts()
+            self.numberHoles = 12
+            self.prepopulateTableView()
         })
         let thirteenButton = UIAlertAction(title: "Thirteen holes", style: .default, handler: {
             (action) -> Void in
             print("Thirteen holes")
-            self.sessionNumber = 13
-            self.showTexts()
+            self.numberHoles = 13
+            self.prepopulateTableView()
         })
         let fourteenButton = UIAlertAction(title: "Fourteen holes", style: .default, handler: {
             (action) -> Void in
             print("Fourteen holes")
-            self.sessionNumber = 14
-            self.showTexts()
+            self.numberHoles = 14
+            self.prepopulateTableView()
         })
         let fifteenButton = UIAlertAction(title: "Fifteen holes", style: .default, handler: {
             (action) -> Void in
             print("Fifteen holes")
-            self.sessionNumber = 15
-            self.showTexts()
+            self.numberHoles = 15
+            self.prepopulateTableView()
         })
         let sixteenButton = UIAlertAction(title: "Sixteen holes", style: .default, handler: {
             (action) -> Void in
             print("Sixteen holes")
-            self.sessionNumber = 16
-            self.showTexts()
+            self.numberHoles = 16
+            self.prepopulateTableView()
         })
         let seventeenButton = UIAlertAction(title: "Seventeen holes", style: .default, handler: {
             (action) -> Void in
             print("Seventeen holes")
-            self.sessionNumber = 17
-            self.showTexts()
+            self.numberHoles = 17
+            self.prepopulateTableView()
         })
         let eighteenButton = UIAlertAction(title: "Eighteen holes", style: .default, handler: {
             (action) -> Void in
             print("Eighteen holes")
-            self.sessionNumber = 18
-            self.showTexts()
+            self.numberHoles = 18
+            self.prepopulateTableView()
         })
         let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: {
             (action) -> Void in
@@ -159,11 +131,29 @@ class AddGolfViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    func showTexts() {
-        createLabel(number: sessionNumber)
-        createTextField(number: sessionNumber)
+    func printTheNumber() {
+        print("The number of holes is equal to: \(self.numberHoles)")
     }
     
+    func prepopulateTableView() {
+        printTheNumber()
+        showTableView()
+        self.numberHolesTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberHoles
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellAdd", for: indexPath) as! AddGolfTableViewCell
+        
+        cell.labHoles.text = "I am label"
+        cell.txtPoint.text = "I am text field"
+        
+        return cell
+    }
+        
     func createLabel( number: Int) {
         let m: Int = 10
         var n: Int = 100
