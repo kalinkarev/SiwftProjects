59031e6e54fabe79416fa93458101a3b2499bfc1
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-08-02 11:33:08 +0300

    Have made the label and text field to show the correct number(using switch)

--- GolfGame/Golf/Golf/AddGolfViewController.swift
+++ GolfGame/Golf/Golf/AddGolfViewController.swift
@@ -21,29 +21,33 @@ class AddGolfViewController: UIViewController, UITableViewDelegate, UITableViewD
         // Do any additional setup after loading the view.
         
         numberHolesTableView.register(AddGolfTableViewCell.self, forCellReuseIdentifier: "cellAdd")
-        
-        numberHolesTableView.isHidden = true
-        nameTextField.isHidden = true
-        
+
+        hideElementOnTheScreen()
+
         showActionSheet()
     }
-    
+
+    func hideElementOnTheScreen() {
+        numberHolesTableView.isHidden = true
+        nameTextField.isHidden = true
+    }
+
     func showElementsOnTheScreen() {
         numberHolesTableView.isHidden = false
         nameTextField.isHidden = false
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     // MARK: Actions
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
         print("You have presed the cancel button")
         dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
         print("You have pressed the save button")
     }
@@ -115,7 +119,7 @@ class AddGolfViewController: UIViewController, UITableViewDelegate, UITableViewD
             (action) -> Void in
             print("Cancel button tapped")
         })
-        
+
         alertController.addAction(nineButton)
         alertController.addAction(tenButton)
         alertController.addAction(elevenButton)
@@ -127,20 +131,20 @@ class AddGolfViewController: UIViewController, UITableViewDelegate, UITableViewD
         alertController.addAction(seventeenButton)
         alertController.addAction(eighteenButton)
         alertController.addAction(cancelButton)
-        
+
         self.present(alertController, animated: true, completion: nil)
     }
-    
+
     func printTheNumber() {
         print("The number of holes is equal to: \(self.numberHoles)")
     }
-    
+
     func prepopulateTableView() {
         printTheNumber()
         showElementsOnTheScreen()
         self.numberHolesTableView.reloadData()
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return numberHoles
     }
@@ -148,25 +152,62 @@ class AddGolfViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellAdd", for: indexPath) as! AddGolfTableViewCell
 
-        var result = UITableViewCell()
-
-        cell.labHoles.text = "I am label"
-        cell.txtPoint.text = "I am text field"
+//        var c: Int = 0
+//
+//        for counter in 0...indexPath.row {
+//            c = counter
+//            c = indexPath.row + 1
+//        }
         
-        result = cell
+        var counter: Int = 0
+        switch indexPath.row {
+        case 0:
+            counter = 1
+        case 1:
+            counter = 2
+        case 2:
+            counter = 3
+        case 3:
+            counter = 4
+        case 4:
+            counter = 5
+        case 5:
+            counter = 6
+        case 6:
+            counter = 7
+        case 7:
+            counter = 8
+        case 8:
+            counter = 9
+        case 9:
+            counter = 10
+        case 10:
+            counter = 11
+        case 11:
+            counter = 12
+        case 12:
+            counter = 13
+        case 13:
+            counter = 14
+        case 14:
+            counter = 15
+        case 15:
+            counter = 16
+        case 16:
+            counter = 17
+        case 17:
+            counter = 18
+        default:
+            counter = 0
+        }
 
-        var i = 0
+//        cell.labHoles.text = "I am label: \(c)"
+//        cell.txtPoint.text = "I am text field: \(c)"
+
+        cell.labHoles.text = "I am label: \(counter)"
+        cell.txtPoint.text = "I am text field: \(counter)"
         
-        repeat {
-            i += 1
-            cell.labHoles.text = "I am label \(i)"
-            cell.txtPoint.text = "I am text field \(i)"
-            
-            print("The i element is: \(i)")
-            
-            return result
-        } while (i < numberHoles)
-
+        return cell
     }
 
     /*
