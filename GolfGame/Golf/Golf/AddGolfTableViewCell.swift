97e342976c6eaeb64e681c6eea895fe851a48a71
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-08-06 15:52:49 +0300

    AddGolfTableViewCell: refactorying the code; Main.storyboard: adding title to the add screen

--- GolfGame/Golf/Golf/AddGolfTableViewCell.swift
+++ GolfGame/Golf/Golf/AddGolfTableViewCell.swift
@@ -12,28 +12,28 @@ class AddGolfTableViewCell: UITableViewCell {
 
     var labHoles = UILabel()
     var txtPoint = UITextField()
-    
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+
         labHoles.translatesAutoresizingMaskIntoConstraints = false
         txtPoint.translatesAutoresizingMaskIntoConstraints = false
-        
+
         contentView.addSubview(labHoles)
         contentView.addSubview(txtPoint)
-        
+
         let viewDict = [
             "holes" : labHoles,
             "points" : txtPoint
         ] as [String : Any]
-        
+
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[holes]-[points]-|", options: [], metrics: nil, views: viewDict))
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -46,15 +46,4 @@ class AddGolfTableViewCell: UITableViewCell {
         selectionStyle = UITableViewCellSelectionStyle.none
     }
 
-    func createLabel() {
-        let myLabel = UILabel(frame: CGRect(x: 10, y: 20, width: 150, height: 35))
-        myLabel.textAlignment = NSTextAlignment.center
-        myLabel.font = myLabel.font.withSize(15)
-    }
-
-    func createTextView() {
-        let myTextView = UITextView(frame: CGRect(x: 150, y: 20, width: 150, height: 35))
-        myTextView.textAlignment = NSTextAlignment.center
-        myTextView.font = .systemFont(ofSize: 13)
-    }
 }
