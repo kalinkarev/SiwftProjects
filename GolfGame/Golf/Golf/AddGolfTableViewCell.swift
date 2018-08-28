daabde0228f60a19fdb15942f90217a3835e7e9e
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-08-28 16:40:42 +0300

    Refactoring the code for the cells in AddGolfTableViewCell and GolfTableViewCell

--- GolfGame/Golf/Golf/AddGolfTableViewCell.swift
+++ GolfGame/Golf/Golf/AddGolfTableViewCell.swift
@@ -9,40 +9,38 @@
 import UIKit
 
 class AddGolfTableViewCell: UITableViewCell {
-    
+
     var labHoles = UILabel()
     var txtPoint = UITextField()
-    
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+
         labHoles.translatesAutoresizingMaskIntoConstraints = false
         txtPoint.translatesAutoresizingMaskIntoConstraints = false
-        
+
         contentView.addSubview(labHoles)
         contentView.addSubview(txtPoint)
-        
+
         let viewDictionary = [
             "holes" : labHoles,
             "points" : txtPoint
         ] as [String : Any]
-        
+
         contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[holes]-[points]-|", options: [], metrics: nil, views: viewDictionary))
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
         selectionStyle = UITableViewCellSelectionStyle.none
     }
 }
--- GolfGame/Golf/Golf/GolfTableViewCell.swift
+++ GolfGame/Golf/Golf/GolfTableViewCell.swift
@@ -11,16 +11,14 @@ import UIKit
 class GolfTableViewCell: UITableViewCell {
     // MARK: Properties
     @IBOutlet weak var nameLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
         selectionStyle = UITableViewCellSelectionStyle.none
     }
 }
