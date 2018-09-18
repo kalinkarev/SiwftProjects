ea74b370952cdd6258f670d81b429594ebb990ed
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-09-18 13:32:36 +0300

    Have changed the size of the elements that are in the cell on adding screen

--- GolfGame/Golf/Golf/AddGolfViewController.swift
+++ GolfGame/Golf/Golf/AddGolfViewController.swift
@@ -200,7 +200,7 @@ extension AddGolfViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellAdd", for: indexPath as IndexPath) as! AddGolfTableViewCell
 
         cell.nameLabelAddScreen.text = "Hole: \(arrayWithHoles[indexPath.row])"
-        cell.pointsTexField.placeholder = "Enter points for hole: \(indexPath.row + 1)"
+        cell.pointsTexField.placeholder = "Points for hole: \(indexPath.row + 1)"
 
         cell.pointsTexField.text = allCellsText[indexPath.row]
         cell.pointsTexField.tag = indexPath.row
