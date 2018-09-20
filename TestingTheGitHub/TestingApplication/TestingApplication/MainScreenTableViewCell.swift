b88c284b7708fc3a4fa341d9f51e17993a29ca05
Kalin Karev <kkarev@Kalins-MacBook-Air.local>
2018-09-20 11:26:36 +0300

    Adding the file that is for the table view cell

---
+++ TestingTheGitHub/TestingApplication/TestingApplication/MainScreenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainScreenTableViewCell.swift
+//  TestingApplication
+//
+//  Created by Kalin Karev on 9/20/18.
+//  Copyright © 2018 Kalin Karev. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
